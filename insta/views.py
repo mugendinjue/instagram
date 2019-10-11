96de0fae504307960821796fd20f3eec3138f5a8
mugendinjue <mugendinjue7@gmail.com>
2019-10-11 14:55:14 +0300

    add login and register view function

--- insta/views.py
+++ insta/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404
+
+def register(request):
+  return render(request,'auth/registration.html')
+
+def login(request):
+  return render(request,'auth/login.html')
+
 
 def index(request):
-  return HttpResponse("lets go")
-
+  return render (request,'main/index.html')
