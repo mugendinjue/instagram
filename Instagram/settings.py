50320eb71ac6996c8a1153164d0c2884e0be44eb
mugendinjue <mugendinjue7@gmail.com>
2019-10-11 14:28:25 +0300

    register bootstrap to installed apps

--- Instagram/settings.py
+++ Instagram/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'insta.apps.InstaConfig'
+    'insta.apps.InstaConfig',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
