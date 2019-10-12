83c9ed6185b9c194dbfee9de85bc117fcada9d53
mugendinjue <mugendinjue7@gmail.com>
2019-10-12 17:32:15 +0300

    add static path to serve images

--- insta/urls.py
+++ insta/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views as main_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
   path('',main_views.register,name='register'),
@@ -9,3 +11,8 @@ urlpatterns = [
   path('logout/',auth_views.LogoutView.as_view(template_name = 'auth/logout.html'),name='logout'),
   path('index/',main_views.index,name='index'),
 ]
+
+if settings.DEBUG:
+  urlpatterns+= static(
+    settings.MEDIA_URL, document_root = settings.MEDIA_ROOT
+  )
