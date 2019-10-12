cf5acf99967ec01002b8dcc836baf9b78e531626
mugendinjue <mugendinjue7@gmail.com>
2019-10-12 08:23:30 +0300

    add a reciever signal to create a profile for users

--- insta/models.py
+++ insta/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 class Image(models.Model):
-  image = models.ImageField(upload_to='instaimages/')
+  image = models.ImageField(upload_to='instaimages/',default='profile/default.jpeg')
   name = models.CharField(max_length=60)
   caption = models.TextField()
   user = models.ForeignKey(User,on_delete = models.CASCADE)
@@ -28,6 +30,16 @@ class Profile(models.Model):
   profile_pic = models.ImageField(upload_to='profile/')
   bio = models.TextField()
   user = models.OneToOneField(User,on_delete = models.CASCADE)
+
+
+  @receiver(post_save , sender = User)
+  def create_profile(instance,sender,created,**kwargs):
+    if created:
+      Profile.objects.create(user = instance)
+
+  @receiver(post_save,sender = User)
+  def save_profile(sender,instance,**kwargs):
+    instance.profile.save()
 
 
   def __str__(self):
