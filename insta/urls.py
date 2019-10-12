ef6ab03dc35d2444c83356b06d9188925e570169
mugendinjue <mugendinjue7@gmail.com>
2019-10-12 11:43:39 +0300

    add a logout,profile and login paths

--- insta/urls.py
+++ insta/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-
-from . import views
+from django.contrib.auth import views as auth_views
+from . import views as main_views
 
 urlpatterns = [
-  path('',views.index,name='index'),
+  path('',main_views.register,name='register'),
+  path('profile/',main_views.profile,name='profile'),
+  path('login/',auth_views.LoginView.as_view(template_name = 'auth/login.html'),name='login'),
+  path('logout/',auth_views.LogoutView.as_view(template_name = 'auth/logout.html'),name='logout'),
+  path('index/',main_views.index,name='index'),
 ]
