df5eca6df28df428e0bb175230faa74c7fbbbe0d
mugendinjue <mugendinjue7@gmail.com>
2019-10-13 10:39:25 +0300

    add a update view function

--- insta/views.py
+++ insta/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import Http404
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from . forms import Registration
+from . forms import Registration,UpdateUser,UpdateProfile
 from django.contrib.auth.decorators import login_required
 from .models import Image
 
@@ -28,6 +28,26 @@ def profile(request):
 
 @login_required
 def index(request):
+  
   images = Image.display_images()
   print(images)
   return render (request,'main/index.html',{"images":images})
+
+@login_required
+def update_profile(request):
+  if request.method == 'POST':
+    u_form = UpdateUser(request.POST,instance=request.user)
+    p_form = UpdateProfile(request.POST,request.FILES,instance=request.user.profile)
+    if u_form.is_valid() and p_form.is_valid():
+      u_form.save()
+      p_form.save()
+      messages.success(request,'Your Profile account has been updated successfully')
+      return redirect('profile')
+  else:
+    u_form = UpdateUser(instance=request.user)
+    p_form = UpdateProfile(instance=request.user.profile) 
+  params = {
+    'u_form':u_form,
+    'p_form':p_form
+  }
+  return render(request,'auth/update_profile.html',params)
