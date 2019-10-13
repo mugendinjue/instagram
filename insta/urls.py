b56be0933aee6f0f7b993a68e0e1fd63278d53d3
mugendinjue <mugendinjue7@gmail.com>
2019-10-13 10:42:37 +0300

    add a profile path'

--- insta/urls.py
+++ insta/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
   path('login/',auth_views.LoginView.as_view(template_name = 'auth/login.html'),name='login'),
   path('logout/',auth_views.LogoutView.as_view(template_name = 'auth/logout.html'),name='logout'),
   path('index/',main_views.index,name='index'),
+  path('update/',main_views.update_profile,name='update_profile'),
 ]
 
 if settings.DEBUG:
