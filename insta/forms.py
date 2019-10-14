fbbe3f280e7f02610ca18cdb119915db2f99c5ef
mugendinjue <mugendinjue7@gmail.com>
2019-10-14 09:07:31 +0300

    add a post image form

--- insta/forms.py
+++ insta/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Profile,Comment
+from .models import Profile,Comment,Image
 
 
 class Registration(UserCreationForm):
@@ -31,5 +31,10 @@ class CommentForm(forms.ModelForm):
   class Meta:
     model = Comment
     fields = ('comment',)
+
+class postImageForm(forms.ModelForm):
+  class Meta:
+    model = Image
+    fields = ['image','name','caption']
 
 
